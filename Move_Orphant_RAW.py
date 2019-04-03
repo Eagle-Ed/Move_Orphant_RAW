7e7ef6a6291e77e6f69715a2bc25ad730889b849
Eagle-Ed <32780841+Eagle-Ed@users.noreply.github.com>
2019-04-03 22:47:48 +0200

    Support for Darktable sidecare files added

--- Move_Orphant_RAW.py
+++ Move_Orphant_RAW.py
@@ -5,17 +5,21 @@
 # -k, -k1. The script will recognize a JPG if the filname equals the
 # filename of a raw file the jpg file may have any addition at the end
 # of it.
-#
+# In addition to the raw file, the script removes Dartable files with
+# the extension .xmp
+# 
 # example:
 # orignial fielname DSC2334.jpg
 # extended filename DSC2334-corrected.jpg
 #
-# !!! Use this script on your own risk !!!
+# !!! Use this script at your own risk !!!
 #
 # Author Eagle Ed
 #
 # Version History
 # 2018-02-10	Version 1.0
+# 2019-01-13    Version 1.1
+#   - remove Darktable files
 #
 import os
 import sys
@@ -23,24 +27,13 @@ from os import walk
 import getopt
 import fnmatch
 import shutil
-
-# add more raw types to your need
-list_of_raw_type = ['.nef', '.cr2']
-dst_folder = 'to_delete'
-
-print ('Raw format supported: '),
-for element in list_of_raw_type:
-    print(element),
-print ('')
+import re
 
 
 def main(argv):
     # verify the arguments
     path_in = ''
     all_args = sys.argv[1:]
-    print '\nall_args ',
-    print all_args
-    print '\n'
     try:
         opts, args = getopt.getopt(sys.argv[1:], "h:p:", ["help=", "path="])
     except getopt.GetoptError:
@@ -55,8 +48,6 @@ def main(argv):
     if (path_in == ''):
         print 'please enter: -p <path'
         sys.exit(2)
-    print ''
-    print path_in + '\n'
     return path_in
 
 
@@ -81,19 +72,22 @@ def create_path(out_path):
     os.makedirs(out_path)
 
 
-def check_for_raw(file_extension):
-    flag_raw = 'false'
-    if file_extension.lower() in list_of_raw_type:
-        flag_raw = 'true'
-    return flag_raw
+def check_filetype(file_extension):
+    file_type = ''
+    if file_extension.lower() in list_of_sidecar_type:
+        file_type = 'sidecar'
+    elif file_extension.lower() in list_of_raw_type:
+        file_type = 'raw'
+    return file_type
 
 
 def search_jpg(file_name, filelist):
-    flag_jpg_exists = 'false'
+    flag_jpg_exists = False
     for files in filelist:
-        search_string = file_name + '*.jpg'
-        if fnmatch.fnmatch(files, search_string):
-            flag_jpg_exists = 'true'
+        search_string = file_name + '.*\.jpe?g$'
+        found_jpg = re.search(search_string, files, re.IGNORECASE)
+        if found_jpg:
+            flag_jpg_exists = True
     return flag_jpg_exists
 
 
@@ -108,32 +102,49 @@ def move_orphant_raw(path_in, filename, dst_dir):
 # store cli arguments
 path_in = main(sys.argv[1:])
 
-# dst_dir = directory where orphand raw are moved to
-dst_dir = os.path.join(path_in, dst_folder)
+# add more raw types to your need
+list_of_raw_type = ['.nef', '.cr2']
+list_of_sidecar_type  = ['.xmp', '.pp3','.on1']
 
+print '\nRaw format supported: ',
+for element in list_of_raw_type:
+    print(element),
+print '\nSidecar format supported: ', 
+for element in list_of_sidecar_type:
+    print(element),
+print
+
+dst_folder = 'to_delete'
+# dst_dir = directory where orphand  raw and sidecar files are moved to
+dst_dir = os.path.join(path_in, dst_folder)
 filelist = list_files_only(path_in)
-orphan_count = 0
+orphant_count = 0
+sidecar_count = 0
 
 for datei in filelist:
-    flag_raw = 'false'
+    flag_raw = False
     # split datei into file_name and file_extension
     file_name, file_extension = os.path.splitext(datei)
-    # check wheter its a raw file or not
-    result = check_for_raw(file_extension)
-    if result == 'true':
-        # if so check if there is a jpg for it
-        result = search_jpg(file_name, filelist)
-        if result == 'false':
+    # figure out file typ (none = image type, raw or sidecar)
+    file_type = check_filetype(file_extension)
+    if file_type == 'raw' or file_type == 'sidecar':
+        if file_type == 'sidecar':
+            # remove the image type extension (.jpg, .cr2, .nef) from the filename
+            file_name = file_name[:len(file_name)-4]
+        jpg_found = search_jpg(file_name, filelist)
+        if not jpg_found:
             # check if the dst_dir exists
             if os.path.exists(dst_dir):
                 if not os.path.isdir(dst_dir):
                     create_path(dst_dir)
             else:
                 create_path(dst_dir)
-            # if not move the raw file to the folder to_delete
-            print('file ' + file_name + file_extension + ' is orphand')
             move_orphant_raw(path_in, datei, dst_dir)
-            orphan_count += 1
+            if file_type == 'sidecar':
+                sidecar_count += 1
+            elif file_type == 'raw':
+                orphant_count += 1
 
-print
-print 'Done - %s orphand raw moved to folder to_delete' % orphan_count
+
+print 'Done\n - %s orphand raw moved to folder to_delete' % orphant_count
+print ' - %s orphand sidecar moved to folder to_delete' % sidecar_count
